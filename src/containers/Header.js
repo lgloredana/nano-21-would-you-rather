9cc01e71af23bb26098f80daf168927681e39c8b
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-02-14 12:20:03 +0200

    refactor: Use statless component for Header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {handleGetAuthedUser} from "../actions/authUser";
 
-class Header extends Component{
-    onLogOut = (e) => {
+const Header = (props) => {
+
+    const onLogOut = (e) => {
         e.preventDefault();
-        this.props.dispatch(handleGetAuthedUser({id: null}))
+        props.dispatch(handleGetAuthedUser({id: null}))
     };
-    render(){
-        const { authedUser } = this.props;
-        return  (
-            <div className='headerContent'>
-                {authedUser ? authedUser.id : ''} /
-                <button onClick={this.onLogOut}>LogOut</button>
-            </div>
-        )
-    }
+    const { authedUser } = props;
+
+    return  (
+        <div className='headerContent'>
+            {authedUser ? authedUser.id : ''} /
+            <button onClick={onLogOut}>LogOut</button>
+        </div>
+    )
 };
 
 function mapStateToProps({authedUser}){
