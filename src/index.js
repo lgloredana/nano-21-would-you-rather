9aae59f6eea1dfba7d20e54ccf48248b4425e073
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-02-22 15:45:19 +0200

    feat: add react dev tool extention

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './containers/App';
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import rootReducer from './reducers/index'
 import {Provider} from "react-redux";
 import thunk from 'redux-thunk';
 import logger from "./middleware/logger";
 
-
-const store = createStore(rootReducer,applyMiddleware(thunk, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose(); //&&
+// window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(
     <Provider store={store}>
