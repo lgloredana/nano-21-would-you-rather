2fdb833bc0d25a5e77063931b65d981151dcdbb1
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-06-13 20:45:07 +0300

    feat: trying to connect to indexDB in order to make the quiz available offline

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import thunk from 'redux-thunk';
 import logger from "./middleware/logger";
 import {composeWithDevTools} from "redux-devtools-extension";
 import {handleGetAuthedUser} from "./actions/authUser";
+import {openDB} from "idb";
 
 const composeEnhancers = composeWithDevTools({
     name: "My nanodegree project",
@@ -17,7 +18,19 @@ const composeEnhancers = composeWithDevTools({
     traceLimit: 23
 });
 // more details about possible options are here: https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md#windowdevtoolsextensionconfig
-const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk, logger)));
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
+
+if (!('indexedDB' in window)) {
+    console.warn('IndexedDB not supported');
+}else{
+    console.log('IndexDB is available!');
+}
+const name = 'fe-guild';
+const version = 3;
+const dbPromise = openDB(name, version, upgradeDB => {
+    console.log('ttttt');
+});
+
 
 ReactDOM.render(
     <Provider store={store}>
