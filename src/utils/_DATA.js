025acf0253615b9378498f83154f4c419697e17b
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-06-14 12:33:50 +0300

    feat: add ids for quizQuestions

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -117,6 +117,7 @@ let questions = {
 
 let quizQuestions ={
   "quiz1" : {
+    id: 'quiz1',
     question: "We are unicorns! Which do you think was the valuation that got us this awesome title?",
     answeright: "USD $1.1 billion",
     answerwrong: "USD $1.5 billion",
@@ -126,6 +127,7 @@ let quizQuestions ={
     }]
   },
   "quiz2" : {
+      id: 'quiz2',
       question: "Where are we headquartered?",
       answeright: "San Francisco",
       answerwrong: "Copenhagen",
@@ -135,12 +137,14 @@ let quizQuestions ={
       }]
   },
     "quiz3" : {
+        id: 'quiz3',
         question: "Did you know we love our employees as much as we love our customers? That's why we got in the top of cloud computing companies to work for. Can you tell how high we were ranked?\n",
         answeright: "Top 10",
         answerwrong: "Top 50",
         users: []
     },
     "quiz4" : {
+        id: 'quiz4',
         question: "We want to grow as much as possible and that's why we also opened an office in China. When did this happen?\n",
         answeright: "2013",
         answerwrong: "2010",
@@ -148,42 +152,49 @@ let quizQuestions ={
     }
     ,
     "quiz5" : {
+        id: 'quiz5',
         question: "We got \"the world's first\" artificial intelligence for the B2B collaboration space. What's her name?\n",
         answeright: "Monica",
         answerwrong: "Ada",
         users: []
     },
     "quiz6" : {
+        id: 'quiz6',
         question: "When did we aquire Babelway, the online B2B integration platform?\n",
         answeright: "2018",
         answerwrong: "2018",
         users: []
     },
     "quiz7" : {
+        id: 'quiz7',
         question: "What happened in 2011?",
         answeright: "Tradeshift made its App marketplace available.",
         answerwrong: "Tradeshift launched Seller Club and Blockchain Payments",
         users: []
     },
     "quiz8" : {
+        id: 'quiz8',
         question: "",
         answeright: "",
         answerwrong: "",
         users: []
     },
     "quiz9" : {
+        id: 'quiz9',
         question: "",
         answeright: "",
         answerwrong: "",
         users: []
     },
     "quizq10" : {
+        id: 'quiz10',
         question: "",
         answeright: "",
         answerwrong: "",
         users: []
     },
     "quiz11" : {
+        id: 'quiz11',
         question: "",
         answeright: "",
         answerwrong: "",
